e09216c8f1860f2a981f6850b5648d61edac3ff2
mohandasss <gamermohan39@gmail.com>
2025-02-14 23:08:33 +0530

    leetcode

--- Happycode/BasicCodes/PerfectNumber.java
+++ Happycode/BasicCodes/PerfectNumber.java
@@ -1,5 +1,30 @@
 public class PerfectNumber {
-    public static void main(String[] args) {
+    void checkPerfect(int numb){
+        int copynumb = numb;
+        int sum=0;
+       for(int i =1;i<copynumb;i++){
+        if (copynumb % i==0) {
+            sum+=i;
+        }
+
+       }
+       
+            
         
+        if(sum==numb)
+
+    {
+        System.out.println("Perfect number");
+
+    }else
+    {
+        System.out.println("not a perfect number");
+    }
+
+    }
+
+    public static void main(String[] args) {
+        PerfectNumber perfectNumber =  new PerfectNumber();
+        perfectNumber.checkPerfect(10 );
     }
 }
