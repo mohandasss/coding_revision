79d1e17798cff3e69b6fb9ebf0a5c65444c1e9b6
mohandasss <gamermohan39@gmail.com>
2025-02-14 23:00:15 +0530

    practicing code

---
+++ Happycode/BasicCodes/SumofDigits.java
@@ -0,0 +1,16 @@
+public class SumofDigits {
+    int sum = 0;
+
+    void sum(int numb){
+        while(numb>0){
+             int digit =numb%10;
+            sum= sum+digit;
+            numb =numb/10;
+        }
+        System.out.println(sum);
+    }
+    public static void main(String args[]){
+            SumofDigits sumofDigits =  new SumofDigits();
+            sumofDigits.sum(563);
+    }
+}
