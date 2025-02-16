6114ec375d09f8bc127219e5fea3987d119bfdea
mohandasss <gamermohan39@gmail.com>
2025-02-17 00:23:01 +0530

    practice session 3

--- Happycode/BasicCodes/SortingSearching/BubbleSort.java
+++ Happycode/BasicCodes/SortingSearching/BubbleSort.java
@@ -1,6 +1,39 @@
 package SortingSearching;
 
+import java.util.Arrays;
+
 public class BubbleSort {
     
-    
+    // Method to perform Bubble Sort
+    public static void bubbleSort(int[] arr) {
+        int n = arr.length;
+        boolean swapped;
+        
+        for (int i = 0; i < n - 1; i++) {
+            swapped = false;
+            
+            // Compare adjacent elements
+            for (int j = 0; j < n - 1 - i; j++) {
+                if (arr[j] > arr[j + 1]) {
+                    // Swap if the current element is greater than the next element
+                    int temp = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = temp;
+                    swapped = true;
+                }
+            }
+            
+            // If no swaps occurred in this pass, the array is already sorted
+            if (!swapped) break;
+        }
+    }
+
+    // Main method to test the sorting algorithm
+    public static void main(String[] args) {
+        int[] arr = {64, 34, 25, 12, 22, 11, 90};
+        
+        System.out.println("Original Array: " + Arrays.toString(arr));
+        bubbleSort(arr);
+        System.out.println("Sorted Array: " + Arrays.toString(arr));
+    }
 }
