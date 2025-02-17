7147f31d9d0ead354e4da88909daadfbd088d276
mohandasss <gamermohan39@gmail.com>
2025-02-17 20:25:40 +0530

    practice session 1

--- Happycode/BasicCodes/SortingSearching/BubbleSort.java
+++ Happycode/BasicCodes/SortingSearching/BubbleSort.java
@@ -3,37 +3,24 @@ package SortingSearching;
 import java.util.Arrays;
 
 public class BubbleSort {
-    
-    
-    public static void bubbleSort(int[] arr) {
-        int n = arr.length;
-        boolean swapped;
-        
-        for (int i = 0; i < n - 1; i++) {
-            swapped = false;
-            
-           
-            for (int j = 0; j < n - 1 - i; j++) {
-                if (arr[j] > arr[j + 1]) {
-                    
-                    int temp = arr[j];
-                    arr[j] = arr[j + 1];
-                    arr[j + 1] = temp;
-                    swapped = true;
+
+    void bubble(int[] arr) {
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = 1; j < arr.length; j++) {
+                if (arr[j] < arr[j - 1]) {
+                    int temp = arr[j - 1];
+                    arr[j - 1] = arr[j];
+                    arr[j] = temp;
                 }
             }
-            
-            
-            if (!swapped) break;
         }
+        System.out.println(Arrays.toString(arr));
     }
 
-    // 
     public static void main(String[] args) {
-        int[] arr = {64, 34, 25, 12, 22, 11, 90};
-        
-        System.out.println("Original Array: " + Arrays.toString(arr));
-        bubbleSort(arr);
-        System.out.println("Sorted Array: " + Arrays.toString(arr));
+        int[] arr = { 2, 56, 78, 5, 6, 8 };
+        BubbleSort bubbleSort = new BubbleSort();
+        bubbleSort.bubble(arr);
     }
+
 }
