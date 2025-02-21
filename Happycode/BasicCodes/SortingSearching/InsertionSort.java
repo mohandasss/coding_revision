b296c31344611255679929bc6a8984241d93f665
mohandasss <gamermohan39@gmail.com>
2025-02-21 22:47:05 +0530

    1st session

--- Happycode/BasicCodes/SortingSearching/InsertionSort.java
+++ Happycode/BasicCodes/SortingSearching/InsertionSort.java
@@ -1,15 +1,26 @@
 package SortingSearching;
-import java.lang.reflect.Array;
-
 import java.util.Arrays;
 
 public class InsertionSort {
-        void insertion(int [] arr){
-                System.out.println(Arrays.toString(arr));
+    void insertion(int[] arr) {
+        int n = arr.length;
+        for (int i = 1; i < n; i++) {
+            int key = arr[i];
+            int j = i - 1;
+            while (j >= 0 && arr[j] > key) {
+                arr[j + 1] = arr[j];
+                j = j - 1;
+            }
+            arr[j + 1] = key;
         }
+        System.out.println(Arrays.toString(arr));
+    }
+
     public static void main(String[] args) {
-        int [] arr = {1,4,5,2,9,3};
+        int[] arr = {1, 4, 5, 2, 9, 3};
         InsertionSort insertionSort = new InsertionSort();
+        System.out.println("Before Sorting: " + Arrays.toString(arr));
         insertionSort.insertion(arr);
+        System.out.println("After Sorting: " + Arrays.toString(arr));
     }
 }
