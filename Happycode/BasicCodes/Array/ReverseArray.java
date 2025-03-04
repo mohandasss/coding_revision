cee905d0df806b7f526723fc619fbd2748fa7a14
mohandasss <gamermohan39@gmail.com>
2025-03-04 21:53:45 +0530

    1st session

---
+++ Happycode/BasicCodes/Array/ReverseArray.java
@@ -0,0 +1,21 @@
+package Array;
+import java.util.Arrays;
+
+public class ReverseArray {
+    public static void reverseArray(int[] arr) {
+        int left = 0, right = arr.length - 1;
+        while (left < right) {
+            int temp = arr[left];
+            arr[left] = arr[right];
+            arr[right] = temp;
+            left++;
+            right--;
+        }
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {1, 2, 3, 4, 5};
+        reverseArray(arr);
+        System.out.println(Arrays.toString(arr)); // Output: [5, 4, 3, 2, 1]
+    }
+}
