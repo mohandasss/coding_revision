3d17f4d1f53ca843506a1949b49eda36a2e32977
mohandasss <gamermohan39@gmail.com>
2025-03-05 23:26:38 +0530

    1st session

--- Happycode/BasicCodes/Array/ReverseArray.java
+++ Happycode/BasicCodes/Array/ReverseArray.java
@@ -16,6 +16,6 @@ public class ReverseArray {
     public static void main(String[] args) {
         int[] arr = {1, 2, 3, 4, 5};
         reverseArray(arr);
-        System.out.println(Arrays.toString(arr)); // Output: [5, 4, 3, 2, 1]
+        System.out.println(Arrays.toString(arr)); 
     }
 }
