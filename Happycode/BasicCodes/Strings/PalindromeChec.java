f36651832bf6d0ca3294b303a85c31575a40903e
mohandasss <gamermohan39@gmail.com>
2025-03-25 23:43:59 +0530

    1st session

---
+++ Happycode/BasicCodes/Strings/PalindromeChec.java
@@ -0,0 +1,13 @@
+package Strings;
+
+public class PalindromeChec {
+    public static boolean isPalindrome(String str) {
+        String reversed = new StringBuilder(str).reverse().toString();
+        return str.equals(reversed);
+    }
+
+    public static void main(String[] args) {
+        String input = "madam";
+        System.out.println("Is Palindrome? " + isPalindrome(input));
+    }
+}
